89d50303e5f2dddffe40b5d2882aa40cfdd605c4
Administrator <Administrator@WIN-PGKGF0DAGS7>
2020-12-21 09:49:51 -0700

    Initial Commit

--- src/Konversi.java
+++ src/Konversi.java
@@ -10,8 +10,8 @@
  */
 import java.util.Scanner;
 public class Konversi {
-    public static void main(String [] a){
-    System.out.println("Pilihan Konversi : \n"
+    public static void main(String[] a) {
+    System.out.println("Pilihan Konversi : \n" 
             + "1. Jam -> Menit \n"
             + "2. Detik -> Hari, Jam, Menit, dan detik"
     );
@@ -19,17 +19,17 @@ public class Konversi {
     System.out.println("\nMasukkan Pilihan");
     
     try (Scanner input = new Scanner(System.in)){
-        if (!input.hasNextInt()){
+        if(!input.hasNextInt()){
         System.out.println("Tidak boleh memasukkan huruf !");
         }
         else{
-        if (input.nextInt() == 1){
+        if(input.nextInt() == 1){
         System.out.println("Masukkan Jam : ");
-        Scanner hours = new Scanner(System.in);
-            jamKeMenit(hours.nextInt());
-        }else {
+        Scanner hours =  new Scanner(System.in);
+           jamKeMenit(hours.nextInt());
+        }else{
         System.out.println("Masukkan Detik : ");
-        try(Scanner seconds = Scanner(System.in)){
+        try(Scanner seconds = new Scanner(System.in)){
         detikKeHari(seconds.nextLong());
         }
         }
@@ -37,29 +37,33 @@ public class Konversi {
     }
     
     }
-
-    public static void jamKeMenit(long jam) {
-    long menit = jam * 60;
-    System.out.println("Menit: " + menit);
     
-    }
+    
+public static void jamKeMenit(long jam){
+long menit = jam * 60;
+System.out.println("Menit: " + menit);
 
-    public static void detikKeHari(long detik) {
-        //hari
-        int hari = (int) (detik / (60 * 60 * 24));
-        
-        //jam
-        detik %= (60 * 60 * 24);
-        int jam = (int) (detik / (60 * 60));
-        
-        //menit
-        detik %= (60 * 60);
-        int menit = (int) (detik / 60);
-        
-        //detik
-        detik %= 60;
-        
-        System.out.println(hari + " Hari, " + jam + " Jam," + menit + " Menit," + detik + " Detik");
-        
-    }
-    }
+}
+
+public static void detikKeHari(long detik){
+    //hari 
+    int hari = (int) (detik / (60 * 60 * 24));
+    
+    //jam
+    detik %= (60 * 60 * 24);
+    int jam = (int) (detik / (60 * 60));
+    
+    //menit
+    detik %= (60 * 60);
+    int menit = (int) (detik / 60);
+    
+    //detik
+    detik %= 60;
+    System.out.println(hari + " Hari, " + jam + " Jam," + menit + " Menit," + detik + " Detik");
+    
+}
+
+    
+    
+    
+}
